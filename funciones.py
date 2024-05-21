28e83f23e88fc1ae57090f53f8193956186841e7
Carlos José González Juan <65392432+Carlosj50@users.noreply.github.com>
2024-05-21 05:24:55 +0100

    Correcciones de agregar tarea

--- funciones.py
+++ funciones.py
@@ -3,23 +3,29 @@ tareas = []
 
 
 def agregar_tareas(lista):
+    
     #Titulo
     print (('\033[1;32m' + '*' * 27 + '\33[;0m').center(100,' '))
     print (('\033[1;31m' + 'Agregar Tarea' + '\33[;0m').center(100,' '))
     print (('\033[1;32m' + '*' * 27 + '\33[;0m').center(100,' '))
+   
     #Entrada para la tarea
     tarea = input('\033[;33m' + 'Escriba la tarea a realizar:\n'+ '\33[;36m')
+   
     #Añadir la tarea al final de la lista
-    tareas.append(tarea)
+    lista.append(tarea)
+  
     #Informe de la tarea añadida
     print ('\033[1;32m' + '*' * 50 + '\33[;0m')
     print ('\033[1;35mSe agrego una nueva tarea\33[;0m')
+   
     #Imprime la tarea añadida
     print ('\033[1;32m' + '*' * 50 + '\33[;0m')
     print ('\033[1;32mSe agrego la siguiente tarea:\n' + tarea + '\33[;0m')
+   
     #Informe del número de tarea
     print ('\033[1;32m' + '*' * 50 + '\33[;0m')
-    print (f'El número de tareas es:  {len(tareas)}')
+    print (f'El número de tareas es:  {len(lista)}')
     input ('\33[;40;37mpulse una tecla\33[;0m')
 
 def ver_tareas(lista):
