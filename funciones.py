4af6bb27a76f8aa17be7fbfb7c0210b80d829311
Carlos José González Juan <65392432+Carlosj50@users.noreply.github.com>
2024-05-21 06:31:12 +0100

    creada la funcion ver tareas

--- funciones.py
+++ funciones.py
@@ -1,5 +1,5 @@
 #lista de tareas
-tareas = []
+tareas = ['tarea 1','tarea 2']
 
 
 def agregar_tareas(lista):
@@ -26,6 +26,8 @@ def agregar_tareas(lista):
     #Informe del número de tarea
     print ('\033[1;32m' + '*' * 50 + '\33[;0m')
     print (f'El número de tareas es:  {len(lista)}')
+
+    #fin de la funcion
     input ('\33[;40;37mpulse una tecla\33[;0m')
 
 def ver_tareas(lista):
@@ -33,6 +35,22 @@ def ver_tareas(lista):
     print (('\033[1;32m' + '*' * 27 + '\33[;0m').center(100,' '))
     print (('\033[1;31m' + 'Ver Tarea' + '\33[;0m').center(100,' '))
     print (('\033[1;32m' + '*' * 27 + '\33[;0m').center(100,' '))
+
+    #Condicional que evalua si hay algo en la lista
+    if lista:
+    #Si hay algo en la lista se presenta
+        for i, e in enumerate(lista):
+            print (f'\033[1;35m {i+1} -. {e}')
+            print ('\033[1;33m' + '*' * 30 + '\33[;0m')
+        #Mensaje de fin de listado
+        print (f'\033[1;34m--- Estas son todas las tareas a realizar el día de hoy ---\n')
+
+    #Si la lista esta vacia se avisa de ello
+    else:
+        print ('\033[1;33m' + '*' * 30 + '\33[;0m')
+        print (f'\33[;31;47m  No hay tareas que realizar.\33[;0m')
+        print ('\033[1;33m' + '*' * 30 + '\33[;0m')
+    #fin de la funcion
     input ('\33[;40;37m' + 'pulse una tecla' + '\33[;0m')
 
 def completar_tareas(lista):
@@ -40,14 +58,18 @@ def completar_tareas(lista):
     print (('\033[1;32m' + '*' * 27 + '\33[;0m').center(100,' '))
     print (('\033[1;31m' + 'Completar Tarea' + '\33[;0m').center(100,' '))
     print (('\033[1;32m' + '*' * 27 + '\33[;0m').center(100,' '))
-    input ('\33[;40;37m' + 'pulse una tecla' + '\33[;0m')
+
+    #fin de la funcion
+    input ('\33[;40;37mpulse una tecla\33[;0m')
 
 def eliminar_tarea(lista):
     #Titulo
     print (('\033[1;32m' + '*' * 27 + '\33[;0m').center(100,' '))
     print (('\033[1;31m' + 'Eliminar Tarea' + '\33[;0m').center(100,' '))
     print (('\033[1;32m' + '*' * 27 + '\33[;0m').center(100,' '))
-    input ('\33[;40;37m' + 'pulse una tecla' + '\33[;0m')
+
+    #fin de la funcion
+    input ('\33[;40;37mpulse una tecla\33[;0m')
 
 def salir():
     salir = input ('\33[;31;47m' + 'Va a salir del programa. ¿Está seguro? s/n \n' + '\33[;0m')
@@ -56,5 +78,6 @@ def salir():
         exit()
     else:
         print ('Continuamos')
-        input ('\33[;40;37m' + 'pulse una tecla' + '\33[;0m')
-        
+
+    #fin de la funcion
+    input ('\33[;40;37mpulse una tecla\33[;0m')
